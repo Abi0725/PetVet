72c1485316b62aadb9a5a12fae4e1c2cfb1625fc
Abi0725 <00086320@uca.edu.sv>
2022-12-01 13:39:22 -0600

    Add

--- proyecto-grupo-de-trabajo-05-petvet-develop/Client/petvet-uca-client/src/Contexts/UserContext.jsx
+++ proyecto-grupo-de-trabajo-05-petvet-develop/Client/petvet-uca-client/src/Contexts/UserContext.jsx
@@ -39,7 +39,7 @@ export const UserContextProvider = (props) => {
 
     const login = async (identifier, password) => {
       try {
-        const { data } = await axios.post("https://api-petvet-production.up.railway.app/auth/signin", { identifier, password });
+        const { data } = await axios.post("https://api-petvet-production.up.railway.app/api/auth/signin", { identifier, password });
         const _token = data.token;
 
         setToken(_token);
